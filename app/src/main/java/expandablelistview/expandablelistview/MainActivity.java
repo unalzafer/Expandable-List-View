bae71b58b44cb01e29e36e848aafb1c730069c08
Ünal Zafer <unalzafer061@gmail.com>
2018-11-16 19:04:30 +0300

    listview expand

---
+++ app/src/main/java/expandablelistview/expandablelistview/MainActivity.java
@@ -0,0 +1,137 @@
+package expandablelistview.expandablelistview;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ExpandableListView;
+import android.widget.ExpandableListView.OnChildClickListener;
+import android.widget.ExpandableListView.OnGroupClickListener;
+import android.widget.ExpandableListView.OnGroupCollapseListener;
+import android.widget.ExpandableListView.OnGroupExpandListener;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    ExpandableListAdapter listAdapter;
+    ExpandableListView expListView;
+    List<String> listDataHeader;
+    HashMap<String, List<String>> listDataChild;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // get the listview
+        expListView = (ExpandableListView) findViewById(R.id.lvExp);
+
+        // preparing list data
+        prepareListData();
+
+        listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
+
+        // setting list adapter
+        expListView.setAdapter(listAdapter);
+
+
+
+        // Listview Group click listener
+        expListView.setOnGroupClickListener(new OnGroupClickListener() {
+
+            @Override
+            public boolean onGroupClick(ExpandableListView parent, View v,
+                                        int groupPosition, long id) {
+                // Toast.makeText(getApplicationContext(),
+                // "Group Clicked " + listDataHeader.get(groupPosition),
+                // Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+
+        // Listview Group expanded listener
+        expListView.setOnGroupExpandListener(new OnGroupExpandListener() {
+
+            @Override
+            public void onGroupExpand(int groupPosition) {
+                Toast.makeText(getApplicationContext(),
+                        listDataHeader.get(groupPosition) + " Menüsü Açıldı",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Listview Group collasped listener
+        expListView.setOnGroupCollapseListener(new OnGroupCollapseListener() {
+
+            @Override
+            public void onGroupCollapse(int groupPosition) {
+                Toast.makeText(getApplicationContext(),
+                        listDataHeader.get(groupPosition) + " Menüsü Kapatıldı",
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        // Listview on child click listener
+        expListView.setOnChildClickListener(new OnChildClickListener() {
+
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v,
+                                        int groupPosition, int childPosition, long id) {
+                // TODO Auto-generated method stub
+                Toast.makeText(
+                        getApplicationContext(),
+                        listDataHeader.get(groupPosition)
+                                + " : "
+                                + listDataChild.get(
+                                listDataHeader.get(groupPosition)).get(
+                                childPosition), Toast.LENGTH_SHORT)
+                        .show();
+                return false;
+            }
+        });
+    }
+
+    /*
+     * Preparing the list data
+     */
+    private void prepareListData() {
+        listDataHeader = new ArrayList<String>();
+        listDataChild = new HashMap<String, List<String>>();
+
+        // Adding child data
+        listDataHeader.add("Süper Lig");
+        listDataHeader.add("Spor Haber");
+        listDataHeader.add("Gazeteler");
+
+        // Adding child data
+        List<String> superLig = new ArrayList<String>();
+        superLig.add("Trabzonspor");
+        superLig.add("Galatasaray");
+        superLig.add("Başiktaş");
+        superLig.add("Fenerbahçe");
+
+
+        List<String> sporHaber = new ArrayList<String>();
+        sporHaber.add("Fanatik");
+        sporHaber.add("Ntv Spor");
+        sporHaber.add("Sporx");
+
+
+        List<String> gazeteler = new ArrayList<String>();
+        gazeteler.add("Hurriyet");
+        gazeteler.add("Milliyet");
+        gazeteler.add("Sabah");
+        gazeteler.add("Akşam");
+        gazeteler.add("Posta");
+
+        listDataChild.put(listDataHeader.get(0), superLig); // Header, Child data
+        listDataChild.put(listDataHeader.get(1), sporHaber);
+        listDataChild.put(listDataHeader.get(2), gazeteler);
+    }
+}
